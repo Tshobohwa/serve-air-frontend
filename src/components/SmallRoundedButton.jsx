cf7bdc3fd1b63062d96776f8bfd8969e0ddd0f4a
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-10-01 20:06:13 +0200

    Make size

--- src/components/SmallRoundedButton.jsx
+++ src/components/SmallRoundedButton.jsx
@@ -4,7 +4,7 @@ const SmallRoundedButton = ({ name, icon, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="w-fit px-[1.2rem] bg-skyblue-800 h-[2.4rem] text-white font-semibold flex gap-4 items-center justify-center rounded-full"
+      className="w-fit px-[1.4rem] bg-skyblue-800 h-[2.8rem] text-white font-semibold flex gap-4 items-center justify-center rounded-full"
     >
       {icon} {name}
     </button>
