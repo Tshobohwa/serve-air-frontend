ffd2f2f6ce1a6727dd6c8b1a8d8f26baddddd98b
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-10-03 03:08:25 +0200

    Start outgoing package

--- src/pages/IncomingPackages.jsx
+++ src/pages/IncomingPackages.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import SmallRoundedButton from "../components/SmallRoundedButton";
 
 const IncomingPackages = () => {
-  return <Sidebar>IncomingPackages</Sidebar>;
+  return (
+    <Sidebar>
+      <header className="w-full flex justify-between items-center">
+        <h1 className="text-3xl font-semibold">Incoming packages</h1>
+      </header>
+    </Sidebar>
+  );
 };
 
 export default IncomingPackages;
--- src/pages/OutgoingPackages.jsx
+++ src/pages/OutgoingPackages.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import SmallRoundedButton from "../components/SmallRoundedButton";
 
 const OutgoingPackages = () => {
-  return <Sidebar>OutgoingPackages</Sidebar>;
+  return (
+    <Sidebar>
+      <header className="w-full flex justify-between items-center">
+        <h1 className="text-3xl font-semibold">Outgoing packages</h1>
+        <SmallRoundedButton name={"new package"} />
+      </header>
+    </Sidebar>
+  );
 };
 
 export default OutgoingPackages;
