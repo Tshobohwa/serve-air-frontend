4eef6ab764c9b8729548321b521ce12158cd1d81
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-10-04 21:47:38 +0200

    Start posting package

--- src/components/TextInputWithLabel.jsx
+++ src/components/TextInputWithLabel.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PhoneInputWithCountrySelect from "react-phone-number-input";
+import "react-phone-number-input/style.css"; // Import the default styles
 
 const TextInputWithLabel = ({
   label,
@@ -12,15 +14,43 @@ const TextInputWithLabel = ({
   return (
     <div className=" w-full my-3 font-outfit">
       <p>{label}</p>
-      <input
-        className="h-[56px] rounded-[12px] border border-skyblue-300 px-4 w-full focus:outline-none placeholder:text-primary-500"
-        type={type}
-        onChange={onChange}
-        placeholder={placeholder}
-        value={value}
-        step={step}
-        readOnly={readOnly}
-      />
+      {type === "tel" ? (
+        <PhoneInputWithCountrySelect
+          international
+          numberInputProps={{
+            className: "focus:outline-none",
+          }}
+          className={
+            "h-[56px] rounded-[12px] border border-skyblue-300 px-4 w-full focus:outline-none placeholder:text-primary-500"
+          }
+          onChange={onChange}
+          placeholder={placeholder}
+          value={value}
+          readOnly={readOnly}
+          defaultCountry="CD"
+          countrySelectProps={{
+            unicodeFlags: true,
+            // Custom rendering for the country select dropdown
+            renderCountrySelect: ({ country, flags }) => (
+              <div className="flex items-center">
+                {flags[country]}
+                <span className="sr-only">{country}</span>{" "}
+                {/* Hides the country code text */}
+              </div>
+            ),
+          }}
+        />
+      ) : (
+        <input
+          className="h-[56px] rounded-[12px] border border-skyblue-300 px-4 w-full focus:outline-none placeholder:text-primary-500"
+          type={type}
+          onChange={onChange}
+          placeholder={placeholder}
+          value={value}
+          step={step}
+          readOnly={readOnly}
+        />
+      )}
     </div>
   );
 };
--- src/popups/NewPackage.jsx
+++ src/popups/NewPackage.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PopupContainer from "./PopupContainer";
 import { IoClose } from "react-icons/io5";
 import TextInputWithLabel from "../components/TextInputWithLabel";
 import RoundedButton from "../components/RoundedButton";
+import { useDispatch, useSelector } from "react-redux";
+import { getRoutes } from "../redux/slices/routesSlice";
+import SelectWithLabel from "../components/SelectWithLabel";
 
 const NewPackage = ({ closeHandler }) => {
+  const dispatch = useDispatch();
+
+  const { routes } = useSelector((state) => state.routes);
+
+  const [sender_name, setSenderName] = useState("");
+  const [sender_phonenumber, setSenderPhonenumber] = useState("");
+  const [receiver_name, setReceiverName] = useState("");
+  const [receiver_phonenumber, setReceiverPhonenumber] = useState("");
+  const [weight, setWeight] = useState("");
+  const [description, setDescription] = useState("");
   const submitHandler = () => {};
+
+  useEffect(() => {
+    dispatch(getRoutes());
+  }, []);
   return (
     <PopupContainer>
       <div className=" pt-[30rem] pb-[10rem] flex items-center justify-center w-full h-full">
@@ -22,11 +39,15 @@ const NewPackage = ({ closeHandler }) => {
               <TextInputWithLabel
                 label={"Name"}
                 placeholder={"Enter sender name"}
+                value={sender_name}
+                onChange={(e) => setSenderName(e.target.value)}
               />
               <TextInputWithLabel
                 label={"Phone number"}
                 placeholder={"Enter sender phone number"}
                 type="tel"
+                onChange={setSenderPhonenumber}
+                value={sender_phonenumber}
               />
             </div>
             <div className="w-full p-4 border border-skyblue-200">
@@ -34,11 +55,15 @@ const NewPackage = ({ closeHandler }) => {
               <TextInputWithLabel
                 label={"Name"}
                 placeholder={"Enter receiver name"}
+                onChange={(e) => setReceiverName(e.target.value)}
+                value={receiver_name}
               />
               <TextInputWithLabel
                 label={"Phone number"}
                 placeholder={"Enter receiver phone number"}
                 type="tel"
+                onChange={setReceiverPhonenumber}
+                value={receiver_phonenumber}
               />
             </div>
             <div className="w-full p-4 border border-skyblue-200">
@@ -47,17 +72,27 @@ const NewPackage = ({ closeHandler }) => {
                 label={"Weight"}
                 placeholder={"Enter package weight"}
                 type="number"
+                onChange={(e) => setWeight(e.target.value)}
+                value={weight}
               />
               <TextInputWithLabel
                 label={"Description"}
                 placeholder={"Enter package description"}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
             </div>
             <div className="w-full p-4 border border-skyblue-200">
               <p className="text-xl font-semibold">Route and pricing</p>
-              <TextInputWithLabel
+              <SelectWithLabel
                 label={"Route"}
+                options={routes}
+                valueExtractor={(route) => route.id}
                 placeholder={"Enter package route"}
+                optionExtractor={(route) =>
+                  `from ${route.origin.address.city} to ${route.destination.address.city}`
+                }
+                keyExtractor={(route) => route.id}
               />
               <TextInputWithLabel
                 label={"Pricing(USD)"}
